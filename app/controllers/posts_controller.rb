61e7f788929daf75cc53ff7cb20a0b286e83749c
jamaspy <aspin@me.com>
2019-04-06 17:48:05 +1100

    added index new edit and show views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,50 @@
+
 class PostsController < ApplicationController
+	 # before_action :find_post, only: [:show, :update, :edit, :destroy]
+
+	def index
+		@posts = Post.all.order("created_at DESC")
+	end
+
+	def new
+		@post = Post.new
+	end
 
 
-  def index
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+
+  def show
+    @post = Post.find(params[:id])
   end
 
-  def new
-  end
+ def update
+   @post = Post.find(params[:id])
 
-  def create
-  end
+   if @post.update(post_params)
+     redirect_to @post
+   else
+     render 'edit'
+ end
+end
 
+ def edit
+   @post = Post.find(params[:id])
+ end
 
-  
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
 end
